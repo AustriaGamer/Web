4f10aeb78620e2542341bd351021aeac1aad4c88
Patrick-Loser <p_loser_Austria@gmx.at>
2021-11-18 11:34:02 +0100

    started modal in todolist

--- js/12-Aufgaben/todolist/todolist.js
+++ js/12-Aufgaben/todolist/todolist.js
@@ -1,24 +1,37 @@
-let todoList = document.getElementById("todo");
-let todoListTasks = [];
-document.getElementById("addTask").addEventListener("click", addToTodoList);
+let todoList ={
+  body:document.getElementById("todo"),
+  tasks : [
+    {
+      name:"Test1",
+      responcebility:"User1",
+      description:"aklfdölkjgfdkjlfdkfdkjfdkk"
+    }
+  ]
+} 
+
+document.getElementById("addTask").addEventListener("click", addTaskToTodoList);
 document.getElementById("taskToAdd").addEventListener("keypress", test);
 function test(e){
     if (e.which == 13) {
-      addToTodoList();
+      addTaskToTodoList();
     }
 }
 
+function openTaskWindow(){
+  document.write("<div id='taskWindow'></div>")
+}
+
 function writeTodoList() {
-  let html = "";
-  for (let i = 0; i < todoListTasks.length; i++) {
-    html +=
+  let htmlOutput = "";
+  for (let i = 0; i < todoList.tasks.length; i++) {
+    htmlOutput +=
       "<div class='task' id='task" +
       i +
       "'> <button class='edit' ></button><li>" +
-      todoListTasks[i] +
+      todoList.tasks[i] +
       "</li><button class='delete' ></button></div>";
   }
-  todoList.innerHTML = html;
+  todoList.body.innerHTML = htmlOutput;
   addButtonFunctionWithClassName("edit");
   addButtonFunctionWithClassName("delete");
 }
@@ -38,20 +51,52 @@ function addButtonFunctionWithClassName(buttonClassName) {
   }
 }
 
-function addToTodoList() {
+function addTaskToTodoList() {
   let task = document.getElementById("taskToAdd").value;
   if (task != "" && task != " ") {
-    todoListTasks.push(task);
+    todoList.tasks.push(task);
     writeTodoList();
   }
   document.getElementById("taskToAdd").value = "";
 }
 
 function editTask(i) {
-  todoListTasks[i] = prompt("Please enter new Title");
+  todoList.tasks[i] = prompt("Please enter new Title");
   writeTodoList();
 }
 function deleteTask(i) {
-  todoListTasks.splice(i, 1); 
+  todoList.tasks.splice(i, 1); 
   writeTodoList();
+}
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("openTaskModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("addTask");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  var taskNames = document.getElementsByClassName("taskNameContainer");
+  taskNames[0].value = todoList.tasks[0].name;
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
