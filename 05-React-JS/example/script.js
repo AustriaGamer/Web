2c712221d16b3c5a8d39606a35da228584b70d10
Patrick-Loser <p_loser_Austria@gmx.at>
2022-02-24 10:50:34 +0100

    finished image-size example

--- 05-React-JS/example/script.js
+++ 05-React-JS/example/script.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
+const sizeOf = require('image-size')
 
-fs.readFile('C:/SWP/swp1-2JG/4cWI/"05 React JS"/example/example.txt', 'utf8' , (err, data) => {
+fs.readFile('C:/SWP/swp1-2JG/4cWI/05-React-JS/example/example.txt', 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
     return
   }
   console.log(data)
 })
+
+const dimensions = sizeOf('C:/SWP/swp1-2JG/4cWI/05-React-JS/example/funny-cats.jpg')
+console.log(dimensions.width, dimensions.height)
