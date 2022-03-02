add2645f5c22593213c1c9ac3111f331ff58b01b
Patrick-Loser <p_loser_Austria@gmx.at>
2022-03-02 11:04:19 +0100

    React Component

---
+++ 05-React-JS/reactdemo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SuperButton from './components/SuperButton';
+
+function App() {
+  return (
+    <div className="App">
+          Hello
+          <SuperButton></SuperButton>
+    </div>
+  );
+}
+
+export default App;
---
+++ 05-React-JS/reactdemo/src/components/SuperButton.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+export default class SuperButton extends Component{
+    render (){
+        return <div>
+         
+          <a class="btn-epic" href='https://www.epicurrence.com/'>Epic Button failed attempt</a>
+            
+  <div>
+    <span> Get Ticket</span>
+    <span> Get Ticket</span>
+    </div>
+ 
+
+        </div>
+    }
+}
