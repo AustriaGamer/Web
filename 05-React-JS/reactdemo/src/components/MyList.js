d28036ec18ce7bf662433003714c685b8a432656
Patrick Loser <p_loser_Austria@gmx.at>
2022-03-05 17:48:19 +0100

    meh

--- 05-React-JS/reactdemo/src/components/MyList.js
+++ 05-React-JS/reactdemo/src/components/MyList.js
@@ -4,22 +4,20 @@ import TextBlock from "./TextBlock.js";
 
 export default function MyList(props) {
   const elements = props.children;
-  
+
   return (
-    <div className={styles.container}>
-        <div className={styles.title}>
-          {props.title}
-        </div>
-        <ul>
-        {elements.map(addKeys)}
-      </ul>
+    <div className={styles.mlcontainer}>
+      <div className={styles.title}>{props.title}</div>
+      <ul className={styles.elements}>{elements.map(addKeys)}</ul>
     </div>
   );
 }
 
-function addKeys(item, index,arr){
-  if(index%2==0){
-    return <TextBlock key={index} text={item} direction="right"/>
+function addKeys(item, index, arr) {
+  if (index % 2 == 0) {
+    return <div key={index} className={styles.elementr}>
+    <TextBlock text={item} direction="right" /></div> ;
   }
-  return <TextBlock key={index} text={item} direction="left"/>
+  return <div key={index} className={styles.elementl}>
+  <TextBlock text={item} direction="left" /></div>;
 }
