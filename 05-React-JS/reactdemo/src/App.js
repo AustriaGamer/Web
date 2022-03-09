97f7f8f8c5c0f3c313bb88796350d5a0d65e569a
Patrick-Loser <p_loser_Austria@gmx.at>
2022-03-09 11:30:19 +0100

    added shadows

--- 05-React-JS/reactdemo/src/App.js
+++ 05-React-JS/reactdemo/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 import SuperButton from './components/SuperButton';
 import TextBlock from './components/TextBlock';
 import MyList from './components/MyList';
 import MyCard from './components/MyCard';
+
 
 function App() {
   return (
@@ -11,7 +12,11 @@ function App() {
           <SuperButton></SuperButton>
           <TextBlock text='Coffee'/>
           <MyList color="red" children={['Tea','Beer','Wine','Vodka','Jack Daniels','Ayran','Red Bull','Whiskey','Gin Tonic','Dom Perignon']} title="My List"/>
+          <div className="rich">
           <MyCard src="./avatar.png" title="Mark Zuckerberg" description="CEO"/>
+          <MyCard src="./avatar.png" title="Elon Musk" description="CEO"/>
+          <MyCard src="./avatar.png" title="Jeff Bezos" description="CEO"/>
+          </div>
     </div>
   );
 }
