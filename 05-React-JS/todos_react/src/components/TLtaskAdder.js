3766caeb042b4ca05d4bc92f42063aba4468d4a4
Patrick-Loser <p_loser_Austria@gmx.at>
2022-03-30 11:27:53 +0200

    modified taskAdder

--- 05-React-JS/todos_react/src/components/TLtaskAdder.js
+++ 05-React-JS/todos_react/src/components/TLtaskAdder.js
@@ -1,17 +1,50 @@
+import { Component } from "react";
 import styles from "./TLtaskAdder.module.css";
 
-export default function TLtaskAdder(props) {
-  return (
-    <div className={styles.taskAdder}>
+export default class TLtaskAdder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputTitle: "",
+      inputText: "",
+    };
+  }
+
+  addTask = () => {
+    this.props.statehandler({ title: "test-title", content: "test-text" })
+  };
+  updateAreaValue = (evt) => {
+    this.setState({
+      inputText: evt.target.value,
+    });
+  };
+  updateInputValue = (evt) => {
+    this.setState({
+      inputTitle: evt.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.taskAdder}>
         <div className={styles.addContent}>
-          <input className={styles.texti}/>
-          <textarea id="" className={styles.texta}/>
+          <input
+            value={this.state.inputTitle}
+            onChange={(evt) => this.updateInputValue(evt)}
+            className={styles.texti}
+          />
+          <textarea
+            value={this.state.inputText}
+            onChange={(evt) => this.updateAreaValue(evt)}
+            className={styles.texta}
+          />
         </div>
-        <button className={styles.addTask}>Add Task</button>
+        <div className={styles.button}>
+          <button className={styles.addTask} onClick={this.addTask}>
+            Add Task
+          </button>
+        </div>
       </div>
-  );
+    );
+  }
 }
-
-
- 
-
