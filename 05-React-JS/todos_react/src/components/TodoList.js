c4ecae2b9d8ab29ba5cce2391f78b8f681d43b61
Patrick-Loser <p_loser_Austria@gmx.at>
2022-03-30 11:27:39 +0200

    modified taskAdder

--- 05-React-JS/todos_react/src/components/TodoList.js
+++ 05-React-JS/todos_react/src/components/TodoList.js
@@ -4,24 +4,28 @@ import TLlist from "./TLlist";
 import TLtaskAdder from "./TLtaskAdder";
 
 export default class TodoList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={tasks:[{title:"test-title", content:"test-text"},{title:"test-title2", content:"test-text2"}],
-    selected:null
+    this.state = {
+      tasks: [
+        { title: "test-title", content: "test-text" },
+        { title: "test-title2", content: "test-text2" },
+      ],
+    };
+    this.stateHandler = this.stateHandler.bind(this);
   }
-  }
-  shwoMore = (row) =>{
-    row.style.display = "visible";
-  }
-  
-  render(){
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>{this.props.title}</div>
-      <TLtaskAdder/>
-      <TLlist elements={this.state.tasks}/>
-    </div>
-  );
+  stateHandler(task){
+    this.setState({tasks:[task]})
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.title}>{this.props.title}</div>
+        <TLtaskAdder statehandler={this.stateHandler} />
+        <TLlist elements={this.state.tasks} />
+      </div>
+    );
   }
 }
-
+// https://stackoverflow.com/questions/26253351/correct-modification-of-state-arrays-in-react-js
